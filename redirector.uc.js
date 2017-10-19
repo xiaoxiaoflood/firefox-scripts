683f6bd0fff9e1d89272ade93b0aa69d2c7b44bb
xiaoxiaoflood <xiaoxiaoflood@gmail.com>
2017-10-19 04:27:51 -0200

    implement nsIChannelEventSink to handle HTTP 3xx redirection
    and rename frameScript to processScript

--- redirector.uc.js
+++ redirector.uc.js
@@ -53,7 +53,7 @@
       '$1'
     ]],
           
-    get frameScript () {
+    get processScript () {
       return 'data:application/javascript;charset=UTF-8,(' + encodeURIComponent(function (rules) {
         var {
           interfaces: Ci,
@@ -64,17 +64,21 @@
         this.policy = {
           contractID: '@xiao/redirector;1',
           classID: Components.ID('{009e4d80-b13f-11e7-8f1a-0800200c9a66}'),
+          xpcom_categories: ['content-policy', 'net-channel-event-sinks'],
 
           init: function () {
             Cm.QueryInterface(Ci.nsIComponentRegistrar).registerFactory(this.classID, 'Redirector', this.contractID, this);
-            XPCOMUtils.categoryManager.addCategoryEntry(['content-policy'], this.contractID, this.contractID, false, true);
+            for (let category of this.xpcom_categories)
+              XPCOMUtils.categoryManager.addCategoryEntry(category, this.contractID, this.contractID, false, true);
           },
           
           destroy: function () {
             Cm.QueryInterface(Ci.nsIComponentRegistrar).unregisterFactory(this.classID, this);
-            XPCOMUtils.categoryManager.deleteCategoryEntry(['content-policy'], this.contractID, false);
+            for (let category of this.xpcom_categories)
+              XPCOMUtils.categoryManager.deleteCategoryEntry(category, this.contractID, false);
           },
 
+          // nsIContentPolicy interface implementation
           shouldLoad: function (contentType, contentLocation, requestOrigin, node) {
             let redirectUrl = this.getRedirectUrl(contentLocation.spec);
             if (contentType == Ci.nsIContentPolicy.TYPE_DOCUMENT && redirectUrl && node) {
@@ -84,13 +88,44 @@
             return Ci.nsIContentPolicy.ACCEPT;
           },
 
+          // nsIChannelEventSink interface implementation
+          asyncOnChannelRedirect: function (oldChannel, newChannel, flags, redirectCallback) {
+            this.onChannelRedirect(oldChannel, newChannel, flags);
+            redirectCallback.onRedirectVerifyCallback(Cr.NS_OK);
+          },
+
+          onChannelRedirect: function (oldChannel, newChannel, flags) {
+              if (!(newChannel.loadFlags & Ci.nsIChannel.LOAD_INITIAL_DOCUMENT_URI))
+                return;
+              let newLocation = newChannel.URI.spec;
+              if (!newLocation)
+                return;
+              let callbacks = [];
+              if (newChannel.notificationCallbacks)
+                callbacks.push(newChannel.notificationCallbacks);
+              if (newChannel.loadGroup && newChannel.loadGroup.notificationCallbacks)
+                callbacks.push(newChannel.loadGroup.notificationCallbacks);
+              let webNav;
+              for (let callback of callbacks) {
+                try {
+                  webNav = callback.getInterface(Ci.nsILoadContext).associatedWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIWebNavigation);
+                  break;
+                } catch(e) {}
+              }
+              if (!webNav)
+                return;
+              let redirectUrl = this.getRedirectUrl(newLocation);
+              if (redirectUrl)
+                webNav.loadURI(redirectUrl, null, null, null, null);
+          },
+
           createInstance: function (outer, iid) {
             if (outer)
               throw Cr.NS_ERROR_NO_AGGREGATION;
             return this.QueryInterface(iid);
           },
 
-          QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy]),
+          QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy, Ci.nsIChannelEventSink]),
 
           rules: rules,
 
@@ -118,7 +153,7 @@
     },
 
     init: function () {
-      Services.ppmm.loadProcessScript(this.frameScript, true);
+      Services.ppmm.loadProcessScript(this.processScript, true);
     },
 
     destroy: function () {
@@ -126,7 +161,7 @@
         this.policy.destroy();
         delete this.policy;
       }).toString() + ')();'), false);
-      Services.ppmm.removeDelayedProcessScript(this.frameScript);
+      Services.ppmm.removeDelayedProcessScript(this.processScript);
       delete Redirector;
     }
   }
