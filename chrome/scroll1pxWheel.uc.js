b0d09566a90a0e482aefbe4d9f8ddb916352ea24
xiaoxiaoflood <xiaoxiaoflood@gmail.com>
2020-12-08 16:40:15 -0300

    scroll1pxWheel bugfixes

--- chrome/scroll1pxWheel.uc.js
+++ chrome/scroll1pxWheel.uc.js
@@ -13,10 +13,14 @@ UC.WheelAutoScroll_1px = {
     win.eval('customElements.get(\'browser\').prototype.startScroll = function ' +
              customElements.get('browser').prototype.startScroll.toString().
                replace(/window\.addEventListener\("DOMMouseScroll", this, true\);/,
-                       `this.handleEventWrapper = (e) => {
-                          this.handleEvent(e, arguments[0]);
-                        };
-                        window.addEventListener("DOMMouseScroll", this.handleEventWrapper, true);`).
+                       `
+      UC.WheelAutoScroll_1px.startar = true;
+      this.handleEventWrapper = (e) => {
+        this.handleEvent(e, arguments[0]);
+      };
+      window.addEventListener("DOMMouseScroll", this.handleEventWrapper, { passive: false, capture: true });\n`).
+               replace(/!this.autoscrollEnabled/,
+                       '!this.autoscrollEnabled || (this._autoScrollPopup?.state == "open" && UC.WheelAutoScroll_1px.startar)').
                replace(/^function /,
                        ''));
     eval('customElements.get(\'browser\').prototype.stopScroll = function ' +
@@ -28,35 +32,49 @@ UC.WheelAutoScroll_1px = {
     eval('customElements.get(\'browser\').prototype.handleEvent = function ' +
          customElements.get('browser').prototype.handleEvent.toString().
            replace(/case "DOMMouseScroll": {\s+[\s\S]+?\n[\s\S]+?\n/,
-                   `case "DOMMouseScroll": {
-                    let data = arguments[1];
-                    if (!this._autoScrollPopup._hidePopup) {
-                      this._autoScrollPopup._hidePopup = this._autoScrollPopup.hidePopup;
-                    }
-                    this._autoScrollPopup.hidePopup = () => {};
-                    setTimeout(() => {
-                      this._autoScrollPopup.hidePopup = this._autoScrollPopup._hidePopup;
-                      this.stopScroll();
-                      this.startScroll(data);
-                    });
-                    gBrowser.selectedBrowser.messageManager.sendAsyncMessage('autoscroll', aEvent.detail);
-                    aEvent.preventDefault();`).
+           `case "DOMMouseScroll": {
+            if (!content) {
+              let data = arguments[1];
+              if (!this._autoScrollPopup._hidePopup) {
+                this._autoScrollPopup._hidePopup = this._autoScrollPopup.hidePopup;
+              }
+              this._autoScrollPopup.hidePopup = () => {};
+              setTimeout(() => {
+                this._autoScrollPopup.hidePopup = this._autoScrollPopup._hidePopup;
+                this.stopScroll();
+                UC.WheelAutoScroll_1px.startar = false;
+                this.startScroll(data);
+              });
+            }
+            this.messageManager.sendAsyncMessage('autoscroll', { d: aEvent.detail, x: aEvent.clientX, y: aEvent.clientY });
+            aEvent.preventDefault();\n`).
            replace(/^function /,
                    ''));
   },
+
+  startar: true,
 
   startScroll_orig: customElements.get('browser').prototype.startScroll.toString().replace(/^function /, ''),
   stopScroll_orig: customElements.get('browser').prototype.stopScroll.toString().replace(/^function /, ''),
   handleEvent_orig: customElements.get('browser').prototype.handleEvent.toString().replace(/^function /, ''),
 
   frameScript: 'data:application/javascript;charset=UTF-8,' + encodeURIComponent('(' + (function () {
+    getScrollParent = el =>
+      el.scrollTop || el.scrollTopMax
+        ? el
+        : el.parentElement
+          ? getScrollParent(el.parentElement)
+          : content.document.scrollingElement
     this.contentListener = (msg) => {
-      if (msg.data < 0) {
-        content.scrollBy(0, -1);
-      } else if (msg.data > 0) {
-        content.scrollBy(0, 1);
-      } else if (msg.data == 'destroy') {
+      if (msg.data.destroy) {
         removeMessageListener('autoscroll', this.contentListener);
+      } else {
+        let el = getScrollParent(content.document.elementFromPoint(msg.data.x, msg.data.y));
+        if (msg.data.d < 0) {
+          el.scrollBy(0, -1);
+        } else if (msg.data.d > 0) {
+          el.scrollBy(0, 1);
+        }
       }
     }
     addMessageListener('autoscroll', this.contentListener);
@@ -68,7 +86,7 @@ UC.WheelAutoScroll_1px = {
 
   destroy: function () {
     Services.mm.removeDelayedFrameScript(this.frameScript);
-    Services.mm.broadcastAsyncMessage('autoscroll', 'destroy');
+    Services.mm.broadcastAsyncMessage('autoscroll', { destroy: true });
     _uc.windows((doc, win) => {
       let customElements = win.customElements;
       let eval = win.eval;
