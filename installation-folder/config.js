ad2061bdaa9a5f77967029741a16614699a99f3a
xiaoxiaoflood <xiaoxiaoflood@gmail.com>
2022-04-09 00:46:18 -0300

    fix config.js

--- installation-folder/config.js
+++ installation-folder/config.js
@@ -19,5 +19,8 @@ try {
   Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
 
   Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
+} catch (ex) {};
+
+try {
   Cu.import('chrome://userchromejs/content/userChrome.jsm');
 } catch (ex) {};
