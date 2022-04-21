xiaoxiaoflood/firefox-scripts

commit 8764048e0397ba9db174344813d674c104805560

do not bind window for "onlyonce" scripts
This was breaking things if you close first browser window.

This minor fix in userChrome.jsm demands some other fixes (declarations) in scripts, because the global object is no longer window.

chrome/rebuild_userChrome.uc.js
@@ -110,17 +110,18 @@ UC.rebuild = {
 	},
 
   clickScriptMenu: function (event) {
-    const { button, ctrlKey, target } = event;
+    const { target } = event;
+    const { closeMenus, gBrowser } = event.view;
     const script = _uc.scripts[target.filename];
-    switch (button) {
+    switch (event.button) {
       case 0:
         this.toggleScript(script);
-        if (ctrlKey)
+        if (event.ctrlKey)
           this.toggleScript(script);
         closeMenus(target);
         break;
       case 1:
-        if (ctrlKey) {
+        if (event.ctrlKey) {
           let url = target.getAttribute('homeURL');
           if (url) {
             gBrowser.addTab(url, { triggeringPrincipal: Services.scriptSecurityManager.createNullPrincipal({}) });
@@ -133,7 +134,7 @@ UC.rebuild = {
         }
         break;
       case 2:
-        if (ctrlKey)
+        if (event.ctrlKey)
           this.uninstall(script);
         else
           this.launchEditor(script);
@@ -310,6 +311,7 @@ UC.rebuild = {
     });
 
     if (AppConstants.MOZ_APP_NAME !== 'thunderbird') {
+      const { CustomizableUI } = window;
       CustomizableUI.createWidget({
         id: 'userChromebtnMenu',
         type: 'custom',

chrome/extensionOptionsMenu.uc.js
@@ -17,6 +17,7 @@ UC.extensionOptionsMenu = {
   blackListArray: [],
 
   init: function() {
+    const { CustomizableUI } = window;
     CustomizableUI.createWidget({
       id: 'eom-button',
       type: 'custom',
@@ -98,32 +99,38 @@ UC.extensionOptionsMenu = {
     });
   },
 
-  handleClick: function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    let win = e.view;
-    let mi = e.target;
-    if (!('_Addon' in mi)) {
+  handleClick: function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const {
+      target: menuitem,
+      view: win
+    } = event;
+
+    const { AddonManager, closeMenus, openURL } = win;
+
+    if (!('_Addon' in menuitem)) {
       return;
     }
 
-    let addon = mi._Addon;
-    let hasMdf = e.ctrlKey || e.shiftKey || e.altKey || e.metaKey;
+    let addon = menuitem._Addon;
+    let hasMdf = event.ctrlKey || event.shiftKey || event.altKey || event.metaKey;
 
-    switch (e.button) {
+    switch (event.button) {
       case 0:
         if (addon.optionsURL && !hasMdf)
           UC.extensionOptionsMenu.openAddonOptions(addon, win);
-        else if (e.ctrlKey)
+        else if (event.ctrlKey)
           UC.extensionOptionsMenu.browseDir(addon);
         break;
       case 1:
         if (addon.homepageURL && !hasMdf) {
           openURL(addon.homepageURL);
-          closeMenus(mi);
-        } else if (e.ctrlKey) {
+          closeMenus(menuitem);
+        } else if (event.ctrlKey) {
           Cc['@mozilla.org/widget/clipboardhelper;1'].getService(Ci.nsIClipboardHelper).copyString(addon.id);
-          closeMenus(mi);
+          closeMenus(menuitem);
         }
         break;
       case 2:
@@ -132,8 +139,8 @@ UC.extensionOptionsMenu = {
             addon.enable();
           else
             addon.disable();
-          UC.extensionOptionsMenu.setDisable(mi, addon, 1);
-        } else if (e.ctrlKey) {
+          UC.extensionOptionsMenu.setDisable(menuitem, addon, 1);
+        } else if (event.ctrlKey) {
           if (Services.prompt.confirm(null, null, 'Delete ' + addon.name + ' permanently?')) {
             if (addon.pendingOperations & AddonManager.PENDING_UNINSTALL)
               addon.cancelUninstall();
@@ -150,8 +157,8 @@ UC.extensionOptionsMenu = {
     }
   },
 
-  setDisable: function (mi, addon, toggling) {
-    let cls = mi.classList;
+  setDisable: function (menuitem, addon, toggling) {
+    let cls = menuitem.classList;
 
     if (addon.operationsRequiringRestart) {
       if (toggling)
@@ -246,7 +253,7 @@ UC.extensionOptionsMenu = {
   },
 
   destroy: function () {
-    CustomizableUI.destroyWidget('eom-button');
+    Services.wm.getMostRecentBrowserWindow().CustomizableUI.destroyWidget('eom-button');
     _uc.sss.unregisterSheet(this.STYLE.url, this.STYLE.type);
     delete UC.extensionOptionsMenu;
   }

chrome/extensionsUpdateChecker.uc.js
@@ -13,6 +13,7 @@ UC.ExtensionsUpdateChecker = {
   OBS_TOPIC_BG_UPD_COMPLETE: 'addons-background-update-complete',
 
   observe: async function () {
+    const { AddonManager, BrowserOpenAddonsMgr } = Services.wm.getMostRecentBrowserWindow();
     let addons = await AddonManager.getAllAddons();
     if (addons.some(addon => addon.updateInstall && !this.ignoreList.includes(addon.name)))
       BrowserOpenAddonsMgr('addons://updates/available');

chrome/masterPasswordPlus.uc.js
@@ -12,7 +12,7 @@ UC.masterPasswordPlus = {
     if (!win.isChromeWindow || win != win.top)
       return;
 
-    let document = win.document;
+    const { document, setTimeout } = win;
 
     let keyset =  _uc.createElement(document, 'keyset', { id: 'masterPassword-keyset' });
     let mainKeyset = document.getElementById('mainKeyset');
@@ -105,7 +105,7 @@ UC.masterPasswordPlus = {
   },
 
   onTitleChanged: function (win) {
-    let document = win.document;
+    const { document, MutationObserver } = win;
     let observer = new MutationObserver(mutationsList => {
       if (mutationsList[0].oldValue !== document.title && document.title != '🞻🞻🞻🞻🞻🞻') {
         win.titulo = document.title;

chrome/minMaxCloseButton.uc.js
@@ -10,6 +10,7 @@
 
 UC.MinMaxCloseButton = {
   init: function () {
+    const { CustomizableUI } = window;
     CustomizableUI.createWidget({
       id: 'minMaxClose-button',
       type: 'custom',
@@ -40,7 +41,7 @@ UC.MinMaxCloseButton = {
         let max = win.document.getElementById('main-window').getAttribute('sizemode') == 'maximized' ? true : false;
         if ((!e.shiftKey && max) ||
             (e.shiftKey && !max && !(win.screenX === -5 && win.screenY === 0 && win.innerWidth === 1992 && win.innerHeight === 1056))) {
-          win.resizeTo(1992, 1056);
+          win.resizeTo(1975, 1052);
           win.moveTo(-5, 0);
         } else if (max && e.shiftKey) {
           win.restore();
@@ -54,7 +55,7 @@ UC.MinMaxCloseButton = {
   },
   
   destroy: function () {
-    CustomizableUI.destroyWidget('minMaxClose-button');
+    Services.wm.getMostRecentBrowserWindow().CustomizableUI.destroyWidget('minMaxClose-button');
     delete UC.MinMaxCloseButton;
   }
 }

chrome/mouseGestures.uc.js
@@ -9,6 +9,7 @@
 
 // initially forked from https://web.archive.org/web/20131025160814/http://www.cnblogs.com/ziyunfei/archive/2011/12/15/2289504.html
 
+const { XPCOMUtils } = ChromeUtils.import('resource://gre/modules/XPCOMUtils.jsm');
 XPCOMUtils.defineLazyGetter(this, 'SelectionUtils', function() {
   let { SelectionUtils } = Cu.import('resource://gre/modules/SelectionUtils.jsm');
   return SelectionUtils;
@@ -722,7 +723,6 @@ UC.MGest = {
         if (this.directionChain) {
           event.preventDefault();
           event.stopPropagation();
-          const { gBrowser } = win;
           this.stopGesture(this.directionChain + (event.deltaY > 0 ? '+' : '-'), win);
         }
         break;
@@ -753,7 +753,7 @@ UC.MGest = {
     Services.mm.removeMessageListener('contentToChrome', this.chromeListener);
 
     _uc.windows((doc, win) => {
-      const { messageManager } = win;
+      const { customElements } = win;
 
       ['contextmenu', 'dragend', 'mousedown', 'mouseup', 'wheel'].forEach(type => {
         doc.removeEventListener(type, this, true);

chrome/multifoxContainer.uc.js
@@ -9,8 +9,7 @@
 
 UC.multifoxContainer = {
   exec: function (win) {
-    let document = win.document;
-    let gBrowser = win.gBrowser;
+    const { document, gBrowser } = win;
 
     let mfcm = _uc.createElement(document, 'menupopup', {
       id: 'mf-contextmenu',
@@ -61,8 +60,8 @@ UC.multifoxContainer = {
   },
 
   showPopup: function (win) {
-    let document = win.document;
-    let userContextId = win.gBrowser.selectedTab.userContextId;
+    const { ContextualIdentityService, document, gBrowser } = win;
+    let userContextId = gBrowser.selectedTab.userContextId;
     let menupopup = document.getElementById('mf-contextmenu');
 
     let firstChild;
@@ -132,7 +131,7 @@ UC.multifoxContainer = {
     _uc.sss.unregisterSheet(this.STYLE.url, this.STYLE.type);
 
     _uc.windows((doc, win) => {
-      let gBrowser = win.gBrowser;
+      const { gBrowser } = win;
       doc.getElementById('mf-contextmenu').remove();
       gBrowser.addTab = gBrowser.orig_addTab;
       delete gBrowser.orig_addTab;

chrome/openInUnloadedTab.uc.js
@@ -12,6 +12,8 @@
 
 // based on https://addons.mozilla.org/en-US/firefox/addon/open-link-in-silent-tab/
 
+const { PlacesUtils } = ChromeUtils.import('resource://gre/modules/PlacesUtils.jsm');
+
 UC.openInUnloadedTab = {
   useLinkAsTabTitle: true, // if Fx can't retrieve title from history or bookmarks (so you never visited the link), should the new tab title be the text link or the url?
 
@@ -81,7 +83,7 @@ UC.openInUnloadedTab = {
     win.SessionStore.setTabState(tab, {
       entries: [{ url: url,
                   title: info?.title || (this.useLinkAsTabTitle && linkText),
-                  triggeringPrincipal_base64: E10SUtils.serializePrincipal(gBrowser.selectedBrowser.contentPrincipal) }],
+                  triggeringPrincipal_base64: win.E10SUtils.serializePrincipal(gBrowser.selectedBrowser.contentPrincipal) }],
       lastAccessed: tab.lastAccessed,
     });
 

commit 5fb12da35e5daefc55c199cf894c039da0f5c2ee

fix rebuild for tb
regressed by 8764048e

chrome/rebuild_userChrome.uc.js
@@ -321,6 +321,7 @@ UC.rebuild = {
         }
       });
     } else {
+      const { document, location } = window;
       const btn = this.createButton(document);
       btn.setAttribute('removable', true);
       const toolbar = document.querySelector('toolbar[customizable=true].chromeclass-toolbar');