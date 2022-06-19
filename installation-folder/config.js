e904ad20ede0fec47f87ddec21291561fb6e0cc4
xiaoxiaoflood <xiaoxiaoflood@gmail.com>
2022-06-19 06:17:41 -0300

    #148 use a better signing bypass method

--- installation-folder/config.js
+++ installation-folder/config.js
@@ -1,8 +1,34 @@
 // skip 1st line
 lockPref('xpinstall.signatures.required', false);
+lockPref('extensions.install_origins.enabled', false);
 
-const { XPIDatabase } = Cu.import('resource://gre/modules/addons/XPIDatabase.jsm');
-XPIDatabase.SIGNED_TYPES.delete('extension');
+// signing bypass by Dumby from forum.mozilla-russia.org
+try {
+  let g = Cu.getGlobalForObject(Cu);
+  let def = g.ChromeUtils.defineModuleGetter;
+  g.ChromeUtils.defineModuleGetter = (...args) => {
+    try {
+      let sandbox = new Cu.Sandbox(g, { freshCompartment: true });
+      Cc['@mozilla.org/jsdebugger;1'].createInstance(Ci.IJSDebugger).addClass(sandbox);
+      let dbg = new sandbox.Debugger();
+      dbg.addDebuggee(g);
+      dbg.makeGlobalObjectReference(g);
+      dbg.addDebuggee(globalThis);
+      let e = dbg.getNewestFrame().older.environment;
+      let obj = e.parent.type == 'object' && e.parent.object;
+      if (obj && obj.class.startsWith('N')) // JSM, NSVO
+        obj.unsafeDereference().Object = {
+          freeze: ac => (ac.MOZ_REQUIRE_SIGNING = false) || g.Object.freeze(ac)
+        };
+      dbg.removeAllDebuggees();
+    } catch(ex) {
+      Cu.reportError(ex);
+    }
+    (g.ChromeUtils.defineModuleGetter = def)(...args);
+  }
+} catch(ex) {
+  Cu.reportError(ex)
+}
 
 try {
   let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
