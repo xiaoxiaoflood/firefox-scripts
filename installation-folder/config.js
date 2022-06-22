7f4e96000baf44398e7308b0aed24781ec29ea82
xiaoxiaoflood <xiaoxiaoflood@gmail.com>
2022-06-22 20:33:32 -0300

    use a simpler code to bypass signing

--- installation-folder/config.js
+++ installation-folder/config.js
@@ -2,33 +2,11 @@
 lockPref('xpinstall.signatures.required', false);
 lockPref('extensions.install_origins.enabled', false);
 
-// signing bypass by Dumby from forum.mozilla-russia.org
-try {
-  let g = Cu.getGlobalForObject(Cu);
-  let def = g.ChromeUtils.defineModuleGetter;
-  g.ChromeUtils.defineModuleGetter = (...args) => {
-    try {
-      let sandbox = new Cu.Sandbox(g, { freshCompartment: true });
-      Cc['@mozilla.org/jsdebugger;1'].createInstance(Ci.IJSDebugger).addClass(sandbox);
-      let dbg = new sandbox.Debugger();
-      dbg.addDebuggee(g);
-      dbg.makeGlobalObjectReference(g);
-      dbg.addDebuggee(globalThis);
-      let e = dbg.getNewestFrame().older.environment;
-      let obj = e.parent.type == 'object' && e.parent.object;
-      if (obj && obj.class.startsWith('N')) // JSM, NSVO
-        obj.unsafeDereference().Object = {
-          freeze: ac => (ac.MOZ_REQUIRE_SIGNING = false) || g.Object.freeze(ac)
-        };
-      dbg.removeAllDebuggees();
-    } catch(ex) {
-      Cu.reportError(ex);
-    }
-    (g.ChromeUtils.defineModuleGetter = def)(...args);
-  }
-} catch(ex) {
-  Cu.reportError(ex)
-}
+// signing bypass by onemen
+const Constants = ChromeUtils.import('resource://gre/modules/AppConstants.jsm');
+const temp = Object.assign({}, Constants.AppConstants);
+temp.MOZ_REQUIRE_SIGNING = false
+Constants.AppConstants = Object.freeze(temp);
 
 try {
   let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
