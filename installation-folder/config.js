51a1b6c47a6e45dc411ba29dd1c017c7b0bf3f26
xiaoxiaoflood <xiaoxiaoflood@gmail.com>
2022-12-24 05:06:00 -0300

    trying a different signing bypass method
     #212

--- installation-folder/config.js
+++ installation-folder/config.js
@@ -8,44 +8,10 @@ try {
   cmanifest.append('chrome.manifest');
   Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
 
-  // signing bypass by Dumby from forum.mozilla-russia.org
-  const g = Cu.getGlobalForObject(Cu);
-  const sandbox = new Cu.Sandbox(g, { freshCompartment: true });
-  Cc['@mozilla.org/jsdebugger;1'].createInstance(Ci.IJSDebugger).addClass(sandbox);
-  const dbg = new sandbox.Debugger();
-  dbg.addDebuggee(g);
-  const gref = dbg.makeGlobalObjectReference(g);
-
-  dbg.onEnterFrame = frame => {
-    const { script } = frame;
-
-    if (!script.url.startsWith('resource://gre/modules/AppConstants.'))
-      return;
-
-    dbg.onEnterFrame = undefined;
-
-    if (script.isModule) { // ESM, Fx 108+
-      const env = frame.environment;
-      frame.onPop = () => env.setVariable('AppConstants', gref.makeDebuggeeValue(Object.freeze(
-        Object.assign(new Object(), env.getVariable('AppConstants').unsafeDereference(), {
-          'MOZ_REQUIRE_SIGNING': false
-        })
-      )));
-    } else { // JSM
-      const nsvo = frame.this.unsafeDereference();
-      nsvo.Object = {
-        freeze (ac) {
-          ac.MOZ_REQUIRE_SIGNING = false;
-          delete nsvo.Object;
-          return Object.freeze(ac);
-        }
-      };
-    }
-  }
-
-  ChromeUtils.import('resource://gre/modules/addons/XPIInstall.jsm');
-
-  dbg.removeAllDebuggees();
+  const Constants = ChromeUtils.import('resource://gre/modules/addons/AddonSettings.jsm');
+  const temp = Object.assign({}, Constants.AddonSettings);
+  temp.REQUIRE_SIGNING = false
+  Constants.AddonSettings = temp;
 
   Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
 } catch (ex) {};
