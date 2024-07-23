bd8ccfb6266a1e9de55aaab309296a65e7e7a78a
xiaoxiaoflood <xiaoxiaoflood@users.noreply.github.com>
2024-07-23 14:25:15 -0300

    fx115

--- installation-folder/config.js
+++ installation-folder/config.js
@@ -3,20 +3,17 @@ lockPref('xpinstall.signatures.required', false);
 lockPref('extensions.install_origins.enabled', false);
 
 try {
-  const cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
+  const cmanifest = Services.dirsvc.get('UChrm', Ci.nsIFile);
   cmanifest.append('utils');
   cmanifest.append('chrome.manifest');
   Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
 
-  const objRef = ChromeUtils.import('resource://gre/modules/addons/AddonSettings.jsm');
-  const temp = Object.assign({}, Object.getOwnPropertyDescriptors(objRef.AddonSettings), {
-    REQUIRE_SIGNING: { value: false }
-  });
-  objRef.AddonSettings = Object.defineProperties({}, temp);
+  const objRef = ChromeUtils.importESModule('resource://gre/modules/addons/XPIDatabase.sys.mjs');
+  objRef.XPIDatabase.SIGNED_TYPES.delete('extension');
 
-  Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
+  ChromeUtils.import('chrome://userchromejs/content/BootstrapLoader.jsm');
 } catch (ex) {};
 
 try {
-  Cu.import('chrome://userchromejs/content/userChrome.jsm');
+  ChromeUtils.import('chrome://userchromejs/content/userChrome.jsm');
 } catch (ex) {};
